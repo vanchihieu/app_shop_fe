01e21b03adad30425a65fdc3b0db404f33b2e0c7
Van Chi Hieu <vanchihieu3@gmail.com>
2024-10-16 14:54:06 +0700

    Hoàn thành quản trị loại sản phẩm

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -88,3 +88,20 @@ export const formatFilter = (filter: any) => {
 
   return result
 }
+
+export const stringToSlug = (str: string) => {
+  // remove accents
+  const from = 'àáãảạăằắẳẵặâầấẩẫậèéẻẽẹêềếểễệđùúủũụưừứửữựòóỏõọôồốổỗộơờớởỡợìíỉĩịäëïîöüûñçýỳỹỵỷ',
+    to = 'aaaaaaaaaaaaaaaaaeeeeeeeeeeeduuuuuuuuuuuoooooooooooooooooiiiiiaeiiouuncyyyyy'
+  for (let i = 0, l = from.length; i < l; i++) {
+    str = str.replace(RegExp(from[i], 'gi'), to[i])
+  }
+
+  str = str
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9\-]/g, '-')
+    .replace(/-+/g, '-')
+
+  return str
+}
