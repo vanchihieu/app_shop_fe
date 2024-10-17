6b7b570753246ee6c164515b1cf3994c4358746d
Van Chi Hieu <vanchihieu3@gmail.com>
2024-10-17 09:13:19 +0700

    Tạo custom date picker component

--- src/pages/manage-product/product/index.tsx
+++ src/pages/manage-product/product/index.tsx
@@ -3,13 +3,14 @@ import { NextPage } from 'next'
 
 // ** Config
 import { PERMISSIONS } from 'src/configs/permission'
+import ProductListPage from 'src/views/pages/manage-product/product/ProductList'
 
 // ** views
 
 type TProps = {}
 
 const Index: NextPage<TProps> = () => {
-  return <h1>User</h1>
+  return <ProductListPage />
 }
 
 Index.permission = [PERMISSIONS.MANAGE_PRODUCT.PRODUCT.VIEW]
